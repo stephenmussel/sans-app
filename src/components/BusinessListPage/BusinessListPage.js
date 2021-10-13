f610865cbddf217e701647c4e0727745ae9c1079
phaydara <phaydara@gmail.com>
2021-10-13 17:04:13 -0500

    captured image click

--- src/components/BusinessListPage/BusinessListPage.js
+++ src/components/BusinessListPage/BusinessListPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import {useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 
 function BusinessList() {
 
@@ -8,22 +9,28 @@ function BusinessList() {
 
   useEffect(() => {
     console.log('in useEffect');
-    
+    // fetches all businesses from business.saga
     const action = {type: 'FETCH_BUSINESS'};
     dispatch(action)
   }, []);
+
+  const viewBusinessDetails = () => {
+    console.log('clicked on image');
+    
+  }
 
   return(
     <div>
       <h1>Business List Page</h1>
       {/* <p>{JSON.stringify(businessList)}</p> */}
-      {businessList.map((each) => (
-        <div>
-          <img src={each.image_url} />
-          <h3>{each.name}</h3>
-        </div>
-
-      ))}
+      {businessList.map(each => {
+        return(
+          <div onClick={viewBusinessDetails} key={each.id}>
+            <img src={each.image_url} />
+            <h3>{each.name}</h3>
+          </div>
+        );
+      })}
     </div>
   )
 }
